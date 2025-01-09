2a2eedd0b8b04fc819afd89f24ba2d6ff8358f0b
eduardevs <epina@itrust.fr>
2025-01-09 08:14:54 +0100

    add new algo structure Stack [WIP]

---
+++ play/Stack.py
@@ -0,0 +1,24 @@
+## uncomment #
+class Stack():
+    def __init__():
+        pass
+    # steps here
+    def push():
+        pass
+    def pop():
+        pass
+    def peek():
+        pass
+## 1 - 
+# print('write list of numbers for bubble sort')
+
+## 2 - input formatting  (for list) 
+#tab_str = input().split(',')
+ 
+## 2.1 - (type of data, list, graph, or....)
+## ex. list
+# tab = [int(num) for num in tab_str]
+
+## 3 - algo function call
+# print(<function algo>(tab))
+
